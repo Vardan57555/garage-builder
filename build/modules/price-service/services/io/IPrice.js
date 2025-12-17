9851835384dc02a67e60d77f47260c2bc0330c9e
Vardan <vardan.babajanyan@reviro.co>
2025-12-17 18:06:45 +0400

    Stop tracking build folder

--- build/modules/price-service/services/io/IPrice.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ValidationTypes = void 0;
-var ValidationTypes;
-(function (ValidationTypes) {
-    ValidationTypes["SINGLE"] = "SINGLE";
-    ValidationTypes["ALL"] = "ALL";
-})(ValidationTypes || (exports.ValidationTypes = ValidationTypes = {}));
-//# sourceMappingURL=IPrice.js.map
